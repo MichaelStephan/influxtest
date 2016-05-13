3ab9736984494156a4eff1fda526c3d2c96f3e89
root <root@ip-10-0-0-152.ec2.internal>
2016-05-13 15:19:43 +0000

    update

--- deploy_perftest.sh
+++ deploy_perftest.sh
@@ -1,1 +1,1 @@
-scp -i influxtest.pem ../influxperftest/release/influxtest-0.1.0-SNAPSHOT-standalone.jar ubuntu@10.0.0.37:
+scp -i influxtest.pem ../influxperftest/release/influxtest-0.1.0-SNAPSHOT-standalone.jar ubuntu@$1:
--- run_perftest.sh
+++ run_perftest.sh
@@ -1,6 +1,6 @@
 KEY=influxtest.pem
 USER=ubuntu
 SSH_HOST=$1
-ssh -i ${KEY} ${USER}@${SSH_HOST} 'JAVA_HOME=~/jdk PATH=${PATH}:~/jdk/bin java -jar influxtest-0.1.0-SNAPSHOT-standalone.jar --database-endpoint http://10.0.0.152:8086 --database testY --n-writers 2 --n-times 100 --batch-size 1000'
+ssh -i ${KEY} ${USER}@${SSH_HOST} 'JAVA_HOME=~/jdk PATH=${PATH}:~/jdk/bin java -jar influxtest-0.1.0-SNAPSHOT-standalone.jar --database-endpoint http://10.0.0.152:8086 --database testY --n-writers 4 --n-times 1000 --batch-size 10000'
 
 
